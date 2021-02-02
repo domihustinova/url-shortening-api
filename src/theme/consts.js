0bbec1599add95dc206083581e092fa64ccd5d0b
domi <dominika.hustinova@kiwi.com>
2021-02-02 19:05:24 +0100

    refactor: add color and font size consts

---
+++ src/theme/consts.js
@@ -0,0 +1,12 @@
+// Colors
+const PRIMARY_CYAN = "hsl(180, 66%, 49%)";
+const PRIMARY_DARK_VIOLET = "hsl(257, 27%, 26%)";
+const SECONDARY_RED = "hsl(0, 87%, 67%)";
+
+const NEUTRAL_GRAY = "hsl(0, 0%, 75%)";
+const NEUTRAL_GRAYISH_VIOLET = "hsl(257, 7%, 63%)";
+const NEUTRAL_VERY_DARK_BLUE = "hsl(255, 11%, 22%)";
+const NEUTRAL_VERY_DARK_VIOLET = "hsl(260, 8%, 14%)";
+
+// Font Sizes
+const MEDIUM = "1.125rem"; // 18px
