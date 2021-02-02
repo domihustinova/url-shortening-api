a20cb5811942fc54d9c933e92dc018e5a927e58e
domi <dominika.hustinova@kiwi.com>
2021-02-02 17:44:33 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      Features Pricing Resources Login Sign Up More than just shorter links
+      Build your brand’s recognition and get detailed insights on how your links
+      are performing. Get Started Shorten a link here... Shorten It! Advanced
+      Statistics Track how your links are performing across the web with our
+      advanced statistics dashboard. Brand Recognition Boost your brand
+      recognition with each click. Generic links don’t mean a thing. Branded
+      links help instil confidence in your content. Detailed Records Gain
+      insights into who is clicking your links. Knowing when and where people
+      engage with your content helps inform better decisions. Fully Customizable
+      Improve brand awareness and content discoverability through customizable
+      links, supercharging audience engagement. Boost your links today Get
+      Started Features Link Shortening Branded Links Analytics Resources Blog
+      Developers Support Company About Our Team Careers Contact
+      <div className="attribution">
+        Challenge by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="https://github.com/domihustinova">Domi</a>.
+      </div>
     </div>
   );
 }
